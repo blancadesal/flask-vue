cf9e626e488d7d611faa4f70b497cf75fe9ce0ad
Slavina Stefanova <sstefanova@wikimedia.org>
2023-09-29 17:54:49 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__, static_folder='dist')
+
+@app.route('/api')
+def hello_world():
+    return {'message': 'Hello, World!'}
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        print(path)
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
+if __name__ == '__main__':
+    app.run()
